3245600303ab44733ed6786687889e7e4424c8b2
pradeep-pasupuleti <pasupuleti.pradeepkumar@gmail.com>
2015-05-16 18:55:28 +0530

    Added plnorm and qlnorm

--- DataSciencePrep/C1/Distributions.R
+++ DataSciencePrep/C1/Distributions.R
@@ -103,3 +103,29 @@ nplot <- function(plist) {
 
 # this is the plot that leads this section.
 nplot(list(p1, p2))
+
+# the 50th percentile (or median) of the lognormal with
+# meanlog=0 and sdlog=10
+qlnorm(0.5)
+# [1] 1
+# the probability of seeing a value x less than 1
+plnorm(1)
+# [1] 0.5
+# the probability of observing a value x less than 10:
+plnorm(10)
+# [1] 0.9893489
+# -- show the 75th percentile of the lognormal
+# use lnormframe from previous example: the
+# theoretical lognormal curve
+line <- qlnorm(0.75)
+xstr <- sprintf("qlnorm(0.75) = %1.3f", line)
+
+lnormframe75 <- subset(lnormframe, lnormframe$x < line)
+# Plot it
+# The shaded area is 75% of the area under the lognormal curve
+ggplot(lnormframe, aes(x=x,y=y)) + geom_line() +
+  geom_area(data=lnormframe75, aes(x=x,y=y), fill="gray") +
+  geom_vline(aes(xintercept=line), linetype=2) +
+  geom_text(x=line, y=0, label=xstr, hjust= 0, vjust=1)
+#As you can see in figure B.5, the majority of the data is concentrated on the left side of
+#the distribution, with the remaining quarter of the data spread out over a very long tail.
